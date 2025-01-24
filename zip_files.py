31d2de53417acdbf94e4612d65adb82588ae46e2
Sofia <sofiaapanansky@gmail.com>
2025-01-24 10:42:06 +0200

    adding and extracting zip files

---
+++ zip_files.py
@@ -0,0 +1,13 @@
+import zipfile
+from pathlib import Path
+
+
+def create_zip_file(filepaths_to_compress: list[str | Path], output_zip: str | Path):
+    with zipfile.ZipFile(output_zip, 'w') as zipf:
+        for file in filepaths_to_compress:
+            zipf.write(file)
+
+
+def extract_zip_file(zip_file_path: str | Path, extract_to: str | Path):
+    with zipfile.ZipFile(zip_file_path, 'r') as zipf:
+        zipf.extractall(extract_to)
