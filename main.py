b4929f31f8c3fb3ae66a23ac5ba53834c7154c3e
Sofia <sofiaapanansky@gmail.com>
2025-01-24 10:58:00 +0200

    update

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import os
 import random
 import string
-import zipfile
 
 from aes_cipher import AESCipher
 from steganography import hide_message_in_image
 from image_split import split_image
 from image_metadata import add_metadata_to_image
+from zip_files import create_zip_file
 
 
 def generate_random_filename(n: int, ext: str = '') -> str:
@@ -16,17 +17,17 @@ def generate_random_filename(n: int, ext: str = '') -> str:
 
 
 def main() -> None:
-    password = input("write password")
+    password = input("Write password: ")
     key = random.randbytes(32)
     cipher = AESCipher(key)
     new_password = cipher.encrypt(password)
 
-    image_path = "poto-1.jpg"
-    new_path = "new_path.png"
+    image_path = "poto-1.jpg" # TODO change
+    new_path = "new_path.png" # TODO change
     hide_message_in_image(image_path, new_password, new_path)
 
-    row = int(input("write row"))
-    cols = int(input("write cols"))
+    row = int(input("Write row: "))
+    cols = int(input("Write cols: "))
     images_matrix = split_image(new_path, row, cols)
 
     filenames = []
@@ -37,7 +38,10 @@ def main() -> None:
             filenames.append(filename)
             add_metadata_to_image(image, f"{i}_{j}", filename)
 
-
+    name_zip_file = generate_random_filename(16, 'zip')
+    create_zip_file(filenames, name_zip_file)
+    for f in filenames:
+        os.remove(f)
 
 
 if __name__ == '__main__':
