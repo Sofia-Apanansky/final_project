690afcf08bac5b8b7437b44ce6469c5749f94a46
Sofia <sofiaapanansky@gmail.com>
2025-02-07 12:04:26 +0200

    Utils for main

---
+++ utils.py
@@ -0,0 +1,28 @@
+import random
+import string
+from pathlib import Path
+
+
+def get_project_directory() -> Path:
+    return Path(__file__).parent
+
+
+def generate_random_filename(n: int, ext: str = '') -> str:
+    name = ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+    if ext:
+        name += '.' + ext
+    return name
+
+
+def is_png_file(file_path: str) -> bool:
+    return file_path.endswith('.png')
+
+
+def row_and_column_to_str(row: int, col: int) -> str:
+    return f'{row}_{col}'
+
+
+def str_to_row_and_column(index_str) -> tuple[int, int]:
+    splitted_indices = index_str.split('_')
+    row, col = splitted_indices
+    return int(row), int(col)
