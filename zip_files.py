315e8ec30cdd84f9b16ceab14063e391585bf54a
Sofia <sofiaapanansky@gmail.com>
2025-03-28 11:18:27 +0300

    fix zip paths

--- zip_files.py
+++ zip_files.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def create_zip_file(filepaths_to_compress: list[str | Path], output_zip: str | Path):
     with zipfile.ZipFile(output_zip, 'w') as zipf:
         for file in filepaths_to_compress:
-            zipf.write(file)
+            zipf.write(filename=file, arcname=Path(file).name)
 
 
 def extract_zip_file(zip_file_path: str | Path, extract_to_folder: str | Path):
