3006c9645cdb96dd90bac55a5c5494e9dbd537d1
Sofia <sofiaapanansky@gmail.com>
2025-04-08 22:15:03 +0300

    Creating the key

--- user_send.py
+++ user_send.py
@@ -1,14 +1,15 @@
 import shutil
 from typing import Final
-
 from aes_cipher import AESCipher
 from random_image import generate_random_image
 from p2p import Peer2Peer
 from steganography import hide_message_in_image
 from image_split import split_image
 from image_metadata import add_metadata_to_image
-from utils import generate_random_filename, row_and_column_to_str, get_project_directory, create_random_name_directory
+from utils import generate_random_filename, row_and_column_to_str, get_project_directory, create_random_name_directory, random_prime_number, int_to_bytes, bytes_to_int
+
 from zip_files import create_zip_file
+from dh_key_exchange import DH_Endpoint
 
 MAX_CONTENT_LENGTH: Final[int] = 115167
 
@@ -18,10 +19,25 @@ def start_user_send(peer_ip: str) -> None:
 
 
 def user_send_loop(peer_ip: str) -> None:
-    # key = random.randbytes(32)  # TODO consider how to generate the key
-    key = b'1' * 32
+    p = random_prime_number()
+    g =random_prime_number()
+    private_key =random_prime_number()
     peer_send = Peer2Peer(peer_ip, 5008, 5007)
-    peer_send.send_message(key)
+
+    send_key=DH_Endpoint(p,g,private_key)
+    public_key= send_key.generate_public_key()
+
+    p=int_to_bytes(p)
+    g=int_to_bytes(g)
+    public_key=int_to_bytes(public_key)
+
+    peer_send.send_message(p)
+    peer_send.send_message(g)
+    peer_send.send_message(public_key)
+
+    key_public_receiver = peer_send.get_message() #public_key_receiver
+    key_public_receiver= bytes_to_int(key_public_receiver)
+    key= int_to_bytes(send_key.generate_full_key(key_public_receiver))
 
     while True:
         temp_directory = create_random_name_directory(16, get_project_directory())
