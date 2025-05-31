e39c0773495578b457476167288703a170e495df
Sofia <sofiaapanansky@gmail.com>
2025-05-31 13:34:58 +0300

    Add comments

--- data_info.py
+++ data_info.py
@@ -3,8 +3,15 @@ from dataclasses import dataclass
 from PIL.ImageFile import ImageFile
 
 
+# SplittedImageInfo is a data container that holds information about a sub-image (tile)
+# from a larger image that has been split into rows and columns.
 @dataclass
 class SplittedImageInfo:
+    # The row index of the sub-image in the original image grid
     row: int
+
+    # The column index of the sub-image in the original image grid
     column: int
+
+    # The actual image tile
     image: ImageFile
